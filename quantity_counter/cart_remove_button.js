8b78ce5eac4061658b875a6d02ee5e3653c04abb
Kaia Bergmann-Dean <kaia@Kaias-MacBook-Air.local>
2026-06-23 17:04:50 -0400

    cart counter

---
+++ quantity_counter/cart_remove_button.js
@@ -0,0 +1,15 @@
+fix('.showcase-quantity-value', (element) => {
+  $(element).closest('.showcase-viewer-cart-items').ATContext({ before: '' });
+  $(element).closest('.showcase-viewer-cart-items').find('.ae-compliance-indent').attr({ 'role': 'status', 'aria-live': 'polite' });
+  let productName = $(element).closest('.showcase-viewer-cart-item-info').find('.showcase-viewer-cart-item-info-title a').text().trim();
+  let extraText = $(element).find('.ae-compliance-indent').text().trim();
+  let productQuant = $(element).text().trim().replace(extraText,'');
+  if (productQuant === '1') {
+    $(element).prev('.showcase-quantity-button.decrease').on('click', (e) => {
+      $(element).watch((ele) => {
+        $(element).closest('.showcase-viewer-cart-items').find('.ae-compliance-indent').text(`${productName} has been removed from cart`);
+        $(element).closest('.showcase-viewer-cart-items').find('.ae-compliance-indent').focusNonInteractive();
+      });
+  });    
+  };
+});
